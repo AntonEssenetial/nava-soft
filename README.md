a376166480ca03df3b2335e407bda0f20d595a7c
Murat Khatypov <antonessential@hotmail.co.uk>
2017-03-27 12:36:24 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Nanomed
+# navatsoft
---
+++ source/modules/select/select.js
@@ -0,0 +1,10 @@
+// custom select
+(function() {
+
+    $('select').styler();
+    $('.jq-selectbox__dropdown ul').mCustomScrollbar({
+        scrollInertia:100,
+        autoExpandScrollbar: true
+    });
+
+})();
